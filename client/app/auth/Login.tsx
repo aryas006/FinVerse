54c117beba250a312b6a0578e465a0e1577cdfc0
mihireshjoshi <mihireshsjoshi@gmail.com>
2024-12-22 06:37:19 +0530

    Login UI

--- client/app/auth/Login.tsx
+++ client/app/auth/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Alert, TouchableOpacity } from 'react-native';
 import { supabase } from '@/supabaseClient';
 import { useRouter } from 'expo-router';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -30,7 +30,7 @@ const Login = ({ navigation }: any) => {
         await AsyncStorage.setItem('username', username || '');  // Storing username
 
         Alert.alert('Login Successful', 'You are now logged in.');
-        
+
         router.push('/Feed/home'); // Navigate to the profile setup screen
       } else {
         throw new Error('Session not found');
@@ -43,6 +43,7 @@ const Login = ({ navigation }: any) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login</Text>
+      <Text style={styles.label}>Email</Text>
       <TextInput
         style={styles.input}
         placeholder="Email"
@@ -51,6 +52,7 @@ const Login = ({ navigation }: any) => {
         keyboardType="email-address"
         autoCapitalize="none"
       />
+      <Text style={styles.label}>Password</Text>
       <TextInput
         style={styles.input}
         placeholder="Password"
@@ -58,15 +60,59 @@ const Login = ({ navigation }: any) => {
         value={password}
         onChangeText={setPassword}
       />
-      <Button title="Login" onPress={handleLogin} />
+      <TouchableOpacity
+        onPress={handleLogin}
+        style={styles.loginButton}
+      >
+        <Text style={styles.loginText}>Login</Text>
+      </TouchableOpacity>
+      
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', padding: 16 },
-  title: { fontSize: 24, marginBottom: 20, textAlign: 'center' },
-  input: { borderWidth: 1, padding: 8, marginBottom: 16, borderRadius: 4 },
+  container: { 
+    flex: 1, 
+    justifyContent: 'center', 
+    padding: 16 
+  },
+  title: { 
+    fontSize: 48, 
+    marginBottom: 20, 
+    textAlign: 'center', 
+    fontFamily: 'Raleway-Medium',
+  },
+  label: {
+    paddingVertical: 6,
+    paddingHorizontal: 8,
+    alignSelf: "flex-start",
+    backgroundColor: "#13375F",
+    borderTopRightRadius: 6,
+    borderTopLeftRadius: 6,
+    color: 'white'
+    
+  },
+  input: { 
+    borderWidth: 1, 
+    padding: 14, 
+    marginBottom: 16, 
+    borderRadius: 4,
+    borderColor: '#13375F'
+  },
+  loginButton: {
+    backgroundColor: "#13375F",
+    paddingVertical: 22,
+    borderRadius: 6,
+    marginTop: 32
+  },
+  loginText: {
+    color: "white",
+    alignItems: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginHorizontal: "auto"
+  }
 });
 
 export default Login;
